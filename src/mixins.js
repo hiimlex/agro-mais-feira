04fa0b4b5fbbb5aaf9933a0c6a3ce89f8502a97a
Francisco lucas (zeta) <lucasnasm@gmail.com>
2019-11-27 14:37:57 -0300

    verifica token

--- src/mixins.js
+++ src/mixins.js
@@ -2,13 +2,10 @@ import { api } from '@/services'
 
 //valida token, mixin created
 export const validaToken = {
-  created() {
+  beforeUpdate() {
     if (window.localStorage.token) {
       api
         .validaToken()
-        .then(response => {
-          this.$store.dispatch("getPerfil")
-        })
         .catch(error => {
           window.localStorage.removeItem('token')
           this.$router.push('entrar')
